0d49a487a17f0f29752a05686653aca06f70cbb8
Akshay Kedlaya <akshaykedlaya@divum.in>
2023-06-30 11:49:16 +0530

    added firebase to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
+import { useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    const firebaseConfig = {
+      apiKey: "AIzaSyAd5fwWXQF5idwlnFmT1I4diU0NEiwTXAw",
+      authDomain: "testfirebase-03.firebaseapp.com",
+      projectId: "testfirebase-03",
+      storageBucket: "testfirebase-03.appspot.com",
+      messagingSenderId: "1042818136390",
+      appId: "1:1042818136390:web:111c40f080f1180ba14d47"
+    };
+    // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  console.log(app);
+  },[])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -16,7 +31,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <h1>Test Firebase</h1>
       </header>
     </div>
   );
